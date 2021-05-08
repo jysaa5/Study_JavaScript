fb4cd594192a4e4c434feec885b83aea0aa8f2a6
jysaa5 <jysaa5@naver.com>
2021-05-09 01:44:47 +0900

    Study JavaScript

--- 20201122_JavaScript/Array_Map.js
+++
@@ -1,9 +0,0 @@
-const arr = [0, 1, 2, 3];
-
-let squaredArr = arr.map(function (element) {
-    return element * element;
-});
-
-squaredArr = arr.map(element => element * element);
-
-console.log(squaredArr);
