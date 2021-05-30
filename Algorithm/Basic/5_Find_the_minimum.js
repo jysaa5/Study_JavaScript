12377290d5ff98d0a38e8b01611397bcca358dd2
jysaa5 <jysaa5@naver.com>
2021-05-30 20:59:48 +0900

    Study JavaScript

---
+++ Algorithm/Basic/5_Find_the_minimum.js
@@ -0,0 +1,8 @@
+// 5. 최솟값 구하기
+// 7개의 수가 주어지면 그 숫자 중 가장 작은 수를 출력하는 프로그램을 작성하세요.
+
+function solution(x1, x2, x3, x4, x5, x6, x7) {
+    return Math.min(x1, x2, x3, x4, x5, x6, x7);
+}
+
+console.log(solution(5, 3, 7, 11, 2, 15, 17));
