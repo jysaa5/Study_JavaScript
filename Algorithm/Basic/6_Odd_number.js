b0eb9831d7bcc7c9a291622ce24d7465b91e57a2
jysaa5 <jysaa5@naver.com>
2021-05-31 20:22:13 +0900

    Update 6_Odd_number.js

--- Algorithm/Basic/6_Odd_number.js
+++ Algorithm/Basic/6_Odd_number.js
@@ -6,3 +6,36 @@
 85 이므로 그 합은 77 + 41 + 53 + 85 = 256 이 되고,
 41 < 53 < 77 < 85 이므로 홀수들 중 최소값은 41 이 된다.
 */
+
+function getOddNumbers(n1, n2, n3, n4, n5, n6, n7) {
+    let arr = [n1, n2, n3, n4, n5, n6, n7];
+    let res = [];
+    for (let num of arr) {
+        if (num % 2 !== 0) {
+            res.push(num);
+        }
+    }
+    return res;
+}
+
+function getSum(arr) {
+    let sum = 0;
+    for (let num of arr) {
+        sum += num;
+    }
+    return sum
+}
+
+
+function solution(n1, n2, n3, n4, n5, n6, n7) {
+    let min = Number.MAX_SAFE_INTEGER;
+    let sum = 0;
+    min = Math.min(...getOddNumbers(n1, n2, n3, n4, n5, n6, n7));
+    sum = getSum(getOddNumbers(n1, n2, n3, n4, n5, n6, n7));
+    return {
+        sum,
+        min
+    };
+}
+
+console.log(solution(12, 77, 38, 41, 53, 92, 85));
