2b066b9711bc23da05f24ed5e7b96f26df387e0f
jysaa5 <57422057+jysaa5@users.noreply.github.com>
2022-02-15 00:20:39 +0900

    study babel

--- Babel/src/babel.config.js
+++ Babel/src/babel.config.js
@@ -1,5 +1,23 @@
 export default function () {
   return {
-    plugins: ["@babel/plugin-transform-block-scoping", "@babel/plugin-transform-arrow-functions", "@babel/plugin-transform-strict-mode"],
+    // plugins: ["@babel/plugin-transform-block-scoping", "@babel/plugin-transform-arrow-functions", "@babel/plugin-transform-strict-mode"],
+    // presets: ["./mypreset.js"],
+    // presets: ["@babel/preset-env"],
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          // targets: {
+          //   chrome: "79",
+          //   ie: "11",
+          // },
+          useBuiltIns: "usage",
+          corejs: {
+            // 폴리필 버전 지정
+            version: 2,
+          },
+        },
+      ],
+    ],
   };
 }
---
+++ Babel/src/mypreset.js
@@ -0,0 +1,5 @@
+export default function () {
+  return {
+    plugins: ["@babel/plugin-transform-arrow-functions", "@babel/plugin-transform-block-scoping", "@babel/plugin-transform-strict-mode"],
+  };
+}
