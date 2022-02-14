09a6d379eb4821e1629b59edf45daa87f08275cd
jysaa5 <57422057+jysaa5@users.noreply.github.com>
2022-02-14 20:14:11 +0900

    Create babel.config.js

---
+++ Babel/src/babel.config.js
@@ -0,0 +1,5 @@
+export default function () {
+  return {
+    plugins: ["@babel/plugin-transform-block-scoping", "@babel/plugin-transform-arrow-functions", "@babel/plugin-transform-strict-mode"],
+  };
+}
