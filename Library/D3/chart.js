0ef2a97bd289b90f3849776b050dfbc2a641d4f6
jysaa5 <jysaa5@naver.com>
2022-04-27 00:34:22 +0900

    D3를 import하기 위해서 URL로 변경

--- Library/D3/chart.js
+++ Library/D3/chart.js
@@ -1,1 +1,21 @@
-import * as d3 from "d3";
+import * as d3 from "https://unpkg.com/d3?module";
+
+const data = [100, 63, 75, 22, 10, 95];
+
+const svg = d3.select("svg").attr("width", 500).attr("height", 300).append("g").attr("transform", `translate(0,0)`);
+
+const xScale = d3
+  .scaleLinear()
+  .range([20, 480])
+  .domain(d3.extent(data, (d, i) => i));
+
+const yScale = d3.scaleLinear().range([280, 20]).domain([0, 100]);
+
+const line = d3
+  .line()
+  .x((d, i) => xScale(i))
+  .y((d) => yScale(d));
+
+console.log(line(data));
+
+svg.append("path").datum(data).attr("fill", "none").attr("stroke", "#76BF8A").attr("stroke-width", 3).attr("d", line);
