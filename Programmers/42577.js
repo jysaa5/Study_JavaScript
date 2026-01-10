bd5624786f0855332a99ad321fee609ef7ce3083
jysaa5 <jysaa5@naver.com>
2026-01-10 23:50:06 +0900

    전화번호 목록 풀이

---
+++ Programmers/42577.js
@@ -0,0 +1,15 @@
+// 전화번호 목록
+
+function solution(phone_book) {
+  let answer = true;
+
+  phone_book.sort();
+
+  for (let i = 0; i < phone_book.length - 1; i++) {
+      if (phone_book[i + 1].startsWith(phone_book[i])) {
+          return false;
+      }
+  }
+
+  return answer;
+}
